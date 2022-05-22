e64a2e524d6d7a1cc33741372dc6c2c3300ea389
Chiara Russo <102867633+chiararusso1@users.noreply.github.com>
2022-05-22 12:44:37 +0200

    caricamento sito
    Caricati file index, forum, about, contact, documentazione (HTML)
    Cartelle CSS, JAVA, immagini

---
+++ java/script.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element CURTAIN MENU */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
